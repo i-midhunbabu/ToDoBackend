56ee764379bd14db39d1c093dc50dcbe2d3b2483
Midhun Babu <98893509+i-midhunbabu@users.noreply.github.com>
2025-06-28 13:44:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,111 @@
+//Backend
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+var cors = require("cors");
+var PORT = process.env.PORT || 8080;
+var MONGOURL = process.env.MONGOURL;
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+)
+
+mongoose.connect(MONGOURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const taskSchema = mongoose.Schema({
+  text: String,
+  status: String, //either may be pending or done
+  priority: String, //less, medium or higher priority
+  userId: mongoose.Schema.Types.ObjectId,
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hashed = await bcrypt(password, 10);
+  const user = new User({ username, password: hashed });
+  await user.save();
+  res.json({ message: "User has been registered" });
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: "Invalid Credentials" });
+  }
+  const token = jwt.sign({ userId: user._id }, "secret", { expiresIn: "1h" });
+  res.json({ token });
+});
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer", "");
+  if (!token) return res.status(401).json({ message: "No Token" });
+  try {
+    const decode = jwt.verify(token, "secret");
+    req.userId = decode.userId;
+    next();
+  } catch (e) {
+    res.status(401).json({ message: "Invalid Token" });
+  }
+};
+
+//Get task request
+app.get("/task", authMiddleware, async (req, res) => {
+  const tasks = await Task.find({ userId: req.userId });
+  res.json(tasks);
+});
+
+//Post task request
+app.post("/task", authMiddleware, async (req, res) => {
+  const task = new Task({ ...req.body,userId: req.userId });
+  await task.save();
+  res.json(task);
+});
+
+//Delete task request
+app.delete("/tasks/:id",authMiddleware,async(req,res)=>{
+  await Task.findOneAndDelete({_id:req.params.id.userId});
+  res.json({message:'Task Deleted'});
+});
+
+//Update status of the task
+app.patch("/tasks/:id/status", authMiddleware,async(req,res)=>{
+  const {status} = req.body;
+  const task = await Task.findOneAndUpdate(
+    {_id:req.params.id, userId:req.userId}, 
+    {status}, 
+    {new: true}
+  );
+  if(!task) return res.status(404).json({message:'Task not found'})
+    res.json(task);
+})
+
+app.patch("/tasks/:id/priority", authMiddleware, async(req,res)=>{
+  const {priority} = req.body;
+  const task = await Task.findOneAndUpdate(
+    {_id:req.params.id, userId: userId},
+    {priority},
+    {new: true}
+  );
+});
+
+
+app.listen(PORT, () => console.log("Server is running on Port:8080"));
