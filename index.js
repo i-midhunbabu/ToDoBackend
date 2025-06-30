ea81bea7cdf68fa28c634042156e8f15670b75af
Midhun Babu <98893509+i-midhunbabu@users.noreply.github.com>
2025-06-30 23:54:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var mongoose = require("mongoose");
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 var cors = require("cors");
+require("dotenv").config();
 var PORT = process.env.PORT || 8080;
 var MONGOURL = process.env.MONGOURL;
 
@@ -14,7 +15,7 @@ app.use(
   cors({
     origin: "*",
   })
-)
+);
 
 mongoose.connect(MONGOURL, {
   useNewUrlParser: true,
@@ -28,7 +29,7 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-const taskSchema = mongoose.Schema({
+const taskSchema = new mongoose.Schema({
   text: String,
   status: String, //either may be pending or done
   priority: String, //less, medium or higher priority
@@ -39,7 +40,7 @@ const Task = mongoose.model("Task", taskSchema);
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const hashed = await bcrypt(password, 10);
+  const hashed = await bcrypt.hash(password, 10);
   const user = new User({ username, password: hashed });
   await user.save();
   res.json({ message: "User has been registered" });
@@ -49,7 +50,7 @@ app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(401).json({ message: "Invalid Credentials" });
+    return res.status(401).json({ message: "Invalid credentials" });
   }
   const token = jwt.sign({ userId: user._id }, "secret", { expiresIn: "1h" });
   res.json({ token });
@@ -59,8 +60,8 @@ const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer", "");
   if (!token) return res.status(401).json({ message: "No Token" });
   try {
-    const decode = jwt.verify(token, "secret");
-    req.userId = decode.userId;
+    const decoded = jwt.verify(token, "secret");
+    req.userId = decoded.userId;
     next();
   } catch (e) {
     res.status(401).json({ message: "Invalid Token" });
@@ -68,44 +69,46 @@ const authMiddleware = (req, res, next) => {
 };
 
 //Get task request
-app.get("/task", authMiddleware, async (req, res) => {
+app.get("/tasks", authMiddleware, async (req, res) => {
   const tasks = await Task.find({ userId: req.userId });
   res.json(tasks);
 });
 
 //Post task request
-app.post("/task", authMiddleware, async (req, res) => {
-  const task = new Task({ ...req.body,userId: req.userId });
+app.post("/tasks", authMiddleware, async (req, res) => {
+  const task = new Task({ ...req.body, userId: req.userId });
   await task.save();
   res.json(task);
 });
 
 //Delete task request
-app.delete("/tasks/:id",authMiddleware,async(req,res)=>{
-  await Task.findOneAndDelete({_id:req.params.id.userId});
-  res.json({message:'Task Deleted'});
+app.delete("/tasks/:id", authMiddleware, async (req, res) => {
+  await Task.findOneAndDelete({ _id: req.params.id, userId: req.userId });
+  res.json({ message: "Task Deleted" });
 });
 
 //Update status of the task
-app.patch("/tasks/:id/status", authMiddleware,async(req,res)=>{
-  const {status} = req.body;
+app.patch("/tasks/:id/status", authMiddleware, async (req, res) => {
+  const { status } = req.body;
   const task = await Task.findOneAndUpdate(
-    {_id:req.params.id, userId:req.userId}, 
-    {status}, 
-    {new: true}
+    { _id: req.params.id, userId: req.userId },
+    { status },
+    { new: true }
   );
-  if(!task) return res.status(404).json({message:'Task not found'})
-    res.json(task);
-})
-
-app.patch("/tasks/:id/priority", authMiddleware, async(req,res)=>{
-  const {priority} = req.body;
-  const task = await Task.findOneAndUpdate(
-    {_id:req.params.id, userId: userId},
-    {priority},
-    {new: true}
-  );
+  if (!task) return res.status(404).json({ message: "Task not found" });
+  res.json(task);
 });
 
+//Update task priority
+app.patch("/tasks/:id/priority", authMiddleware, async (req, res) => {
+  const { priority } = req.body;
+  const task = await Task.findOneAndUpdate(
+    { _id: req.params.id, userId: req.userId },
+    { priority },
+    { new: true }
+  );
+  if (!task) return res.status(404).json({ message: "Task not found" });
+  res.json(task);
+});
 
 app.listen(PORT, () => console.log("Server is running on Port:8080"));
